4f23eeb87c6c391af80763b965c57ab16fbbd92e
Marlon Monge <crinxaos@gmail.com>
2026-04-18 20:07:46 -0600

    Initial commit - Nova Landing Page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    document.getElementById("formMessage").innerText =
+        "Message sent successfully (demo mode)";
+});
